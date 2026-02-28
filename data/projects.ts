bf7c36ecd249efd1bad4f1d2af4d4b9ddb951cb1
Dat Le <ldat0909@gmail.com>
2026-02-28 19:26:59 +0700

    Removed JumpyBird

--- data/projects.ts
+++ data/projects.ts
@@ -100,19 +100,19 @@ export const projectsData: ProjectData[] = [
     themeColor: "#F472B6",
     color: "#F472B6"
   },
-  {
-    id: "jumpybird",
-    title: "Jumpy Bird",
-    shortDesc: "A Flappy Bird clone Web game",
-    description: "Jumpy Bird is a clone of the famous game built with Cocos Creator to fulfill my passion for games.",
-    techStack: ["Cocos Creator, TypeScript"],
-    images: [JumpyBirdGif],
-    links: {
-      github: "https://github.com/datle04/jumpy-bird",
-    },
-    themeColor: "#a3e635",
-    color: "#a3e635"
-  },
+  // {
+  //   id: "jumpybird",
+  //   title: "Jumpy Bird",
+  //   shortDesc: "A Flappy Bird clone Web game",
+  //   description: "Jumpy Bird is a clone of the famous game built with Cocos Creator to fulfill my passion for games.",
+  //   techStack: ["Cocos Creator, TypeScript"],
+  //   images: [JumpyBirdGif],
+  //   links: {
+  //     github: "https://github.com/datle04/jumpy-bird",
+  //   },
+  //   themeColor: "#a3e635",
+  //   color: "#a3e635"
+  // },
 ];
 
 export const getProjectById = (id: string) => projectsData.find(p => p.id === id);
